0968ddec3d81a1924a1eb25c852a7547659dff7e
Gersson0410 <ghuichi@unsa.edu.pe>
2025-07-22 00:36:27 -0500

    Prueba Automatizada - 05 - Administrador

--- Automatizacion_Administrador/CPF-ADM-005.py
+++ Automatizacion_Administrador/CPF-ADM-005.py
@@ -1,10 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.action_chains import ActionChains
 from dotenv import load_dotenv
 import os
 import time
@@ -43,13 +43,14 @@ class TestADM005:
     def test_adm005(self):
         self.driver.get("https://teammates-hormiga-1.uc.r.appspot.com/web/front/home")
 
+        # Menú responsive (si aplica)
         try:
             toggler = self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".navbar-toggler-icon")))
             toggler.click()
         except TimeoutException:
             pass
 
-        # Login como instructor
+        # Login como instructor con Google
         self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".nav-item:nth-child(1) > .dropdown-toggle"))).click()
         self.wait.until(EC.element_to_be_clickable((By.ID, "instructor-login-btn"))).click()
 
@@ -61,18 +62,32 @@ class TestADM005:
         password_input.send_keys(self.password)
         password_input.send_keys(Keys.ENTER)
 
-        self.wait.until(EC.presence_of_element_located((By.LINK_TEXT, "Home")))
+        # Esperar que cargue vista de instructor
+        try:
+            self.wait.until(EC.presence_of_element_located((By.LINK_TEXT, "Home")))
+            self.driver.save_screenshot("01_home_instructor.png")
+        except TimeoutException:
+            self.driver.save_screenshot("error_login.png")
+            raise Exception("No se cargó correctamente la vista de instructor.")
 
-        # Ir a "To admin pages"
+        # Ir al menú de usuario (último dropdown)
         dropdowns = self.driver.find_elements(By.CSS_SELECTOR, ".dropdown-toggle")
         if dropdowns:
             dropdowns[-1].click()
         else:
             raise Exception("No se encontró el menú desplegable del usuario.")
 
+        # Ir a página admin
         self.wait.until(EC.element_to_be_clickable((By.LINK_TEXT, "To admin pages"))).click()
-        self.wait.until(EC.element_to_be_clickable((By.LINK_TEXT, "Search"))).click()
+        self.wait.until(EC.url_contains("/admin"))
+        self.driver.save_screenshot("02_admin_dashboard.png")
 
+        # Ir a "Search"
+        self.wait.until(EC.element_to_be_clickable((By.LINK_TEXT, "Search"))).click()
+        self.wait.until(EC.url_contains("/admin/search"))
+        self.driver.save_screenshot("03_admin_search.png")
+
+        # Entradas a probar
         entradas = [
             "ghuichi@unsa.edu.pe",   # válida
             "noexiste123",           # no existente
